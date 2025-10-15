daa79186b4e27cdc6d9d9c35146f56fb9f18ca37
Enzo Loungoundji <enzo.loungoundji@etudiant.univ-reims.fr>
2025-10-15 12:38:55 +0200

    feat: ajout de data

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+const projects = [
+  {
+    id: 1,
+    title: 'Application météo',
+    description: 'Une app météo avec React et API OpenWeather.',
+    image: '/assets/meteo.jpg',
+    github: 'https://github.com/tonprofil/meteo-app',
+    demo: 'https://meteo-tonprofil.vercel.app/',
+  },
+  {
+    id: 2,
+    title: 'Todo List',
+    description: 'Une Todo List moderne avec React et Tailwind.',
+    image: '/assets/todo.jpg',
+    github: 'https://github.com/tonprofil/todo-app',
+  },
+  {
+    id: 3,
+    title: 'Portfolio précédent',
+    description: 'Ancienne version de mon portfolio web personnel.',
+    image: '/assets/portfolio.jpg',
+    github: 'https://github.com/tonprofil/portfolio-v1',
+  },
+]
+
+export default projects
