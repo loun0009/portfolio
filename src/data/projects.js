e1f07a6beff94200c65e6f639edd6a3abadde90e
Enzo Loungoundji <enzo.loungoundji@etudiant.univ-reims.fr>
2025-10-22 21:21:29 +0200

    feat: ajout + import des images

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,29 +1,34 @@
+import cinema from '../assets/cinema.png';
+import snippets from '../assets/snippets.png';
+import manager from '../assets/manager.png';
+import gestion from '../assets/gestion.png';
+
 const projects = [
   {
     title: "Gestionnaire d'emploi du temps",
     description: "Projet scolaire en groupe d'application web de gestion d'emploi du temps de professeur grâce à l'importation d'un tableau excel, ou par attribution de cours sur l'application.",
-    image: 'https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: gestion,
     tags: ['JavaScript','React', 'PHP', 'Symfony', 'Tailwind'],
     github: 'https://github.com/loun0009/Projet-gestion',
   },
   {
     title: 'Liste de snippets',
     description: 'Petit site web pour la decouverte de TypeScript et Prisma, le site permet de consulter et de rédiger des petits extrait de code comme des articles. ',
-    image: 'https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: snippets,
     tags: ['Javascript', 'TypeScript', 'CSS', 'Prisma'],
     github: 'https://github.com/loun0009/Snippets',
   },
   {
     title: 'Catalogue de film',
     description: 'Projet scolaire en binôme, voici un site un en PHP qui répertorie des films depuis une base de données avec leur synopsis et acteur principaux avec un style imposé. ',
-    image: 'https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: cinema,
     tags: ['PHP', 'CSS'],
     github: 'https://github.com/loun0009/Catalogue-film',
   },
   {
     title: 'Gitlab manager',
     description: 'Application web de gestion de projet Gitlab, permettant de visualiser les projets, les membres, les issues et les merge requests d\'un groupe Gitlab via l\'API Gitlab.',
-    image: 'https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: manager,
     tags: ['JavaScript', 'React Native','Expo', 'Gitlab API'],
     github: 'https://github.com/loun0009/gitlab-manager',
   }
